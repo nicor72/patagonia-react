d2de665a306f8b1e204f82d51b876d0c6300520f
nico <nroman@eclass.cl>
2019-03-03 11:18:08 -0300

    test(App): test only dev deploy

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            COMMING SOON!
+            EN DESARROLLO
           </p>
           <a
             className="App-link"
